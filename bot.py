3e8cf67dc2e7db03b412ba7548e427564d737bac
Mitchell Stewart <mitchell.stewart08@gmail.com>
2020-08-07 18:46:21 -0500

    bot makes successful http requests to dummy server

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 import os
+import aiohttp
+import asyncio
 from dotenv import load_dotenv
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -8,18 +11,28 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
 
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
     )
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+
+    if message.content.startswith("!covid"):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://jsonplaceholder.typicode.com/posts') as r:
+                if r.status == 200:
+                    js = await r.json()
+                    print(f'{js}')
+                else:
+                    await message.channel.send("Error")
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
